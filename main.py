b3a9d75fe6a277fa7658ddcb8289603bebc06078
ninoko1995 <ninoninomiyamiya1995@gmail.com>
2022-05-24 18:39:57 +0900

    create test

---
+++ main.py
@@ -0,0 +1,18 @@
+import pickle
+from models.car import Car
+
+
+def save_car() -> None:
+    max_speed = 100
+    car = Car(max_speed)
+    car.print_max_speed()
+    car.save()
+
+def load_car() -> None:
+    car = Car.restore()
+    print(car.speedometer.max_speed)
+
+if __name__ == '__main__':
+    save_car()
+
+    # load_car()
---
+++ models/car.py
@@ -0,0 +1,30 @@
+import pickle
+
+class Speedometer:
+    # def __init__(self) -> None:
+    #     self.max_speed = "dummy"
+
+    def __init__(self, max_speed: int) -> None:
+        self.max_speed = max_speed
+    
+    def print_max_speed(self) -> None:
+        print("this is from Speedometer")
+        print(self.max_speed)
+
+class Car:
+    def __init__(self, max_speed: int) -> None:
+        self.speedometer = Speedometer(max_speed)
+        # self.max_speed = max_speed
+
+    def print_max_speed(self) -> None:
+        self.speedometer.print_max_speed()
+        # print(self.max_speed)
+
+    def save(self) -> None:
+        with open('pickle.pkl', 'wb') as f:
+            pickle.dump(self, f)
+
+    @classmethod
+    def restore(cls) -> 'Car':
+        with open('pickle.pkl', 'rb') as f:
+            return pickle.load(f)
